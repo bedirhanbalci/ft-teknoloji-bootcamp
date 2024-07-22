44e171621172354e888f36ebce0a13eb7bb7c6f3
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-07-22 03:04:35 +0300

    Added Main class to demonstrate HouseService functionalities
    - Created Main class to test and display HouseService methods
    - Added functionality to display total prices for houses, villas, and summer houses
    - Implemented display of average square meters for different house types
    - Added filtering of houses based on number of rooms and living rooms

--- real-estate/src/main/java/com/patika/Main.java
+++ real-estate/src/main/java/com/patika/Main.java
@@ -1,7 +1,31 @@
 package com.patika;
 
+import com.patika.model.House;
+import com.patika.repository.HouseRepository;
+import com.patika.service.HouseService;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HouseRepository houseRepository = new HouseRepository();
+
+        HouseService houseService = new HouseService(houseRepository);
+
+        System.out.println("Evlerin toplam fiyatı: " + houseService.getTotalPriceOfHouses());
+        System.out.println("Villaların toplam fiyatı: " + houseService.getTotalPriceOfVillas());
+        System.out.println("Yazlık evlerin toplam fiyatı: " + houseService.getTotalPriceOfSummerHouses());
+        System.out.println("Tüm evlerin toplam fiyatı: " + houseService.getTotalPriceOfAllHouses());
+
+        System.out.println("Evlerin ortalama metrekaresi: " + houseService.getAverageSquareMetersOfHouses());
+        System.out.println("Villaların ortalama metrekaresi: " + houseService.getAverageSquareMetersOfVillas());
+        System.out.println("Yazlık evlerin ortalama metrekaresi: " + houseService.getAverageSquareMetersOfSummerHouses());
+        System.out.println("Tüm evlerin ortalama metrekaresi: " + houseService.getAverageSquareMetersOfAllHouses());
+
+        int rooms = 5;
+        int livingRooms = 2;
+        List<House> filteredHouses = houseService.filterHousesByRoomAndLivingRoom(rooms, livingRooms);
+        System.out.println(rooms + " oda ve " + livingRooms + " salon bulunan filtrelenmiş evler: " + filteredHouses);
     }
 }
